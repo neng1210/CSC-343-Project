2db2d0193a96e0328318b2e21575995f09d7af14
RyuFunakoshiya <rfunakoshiya@email.arizona.edu>
2022-04-13 21:00:42 -0700

    Added iframe
    The main body can now swap HTML files.

--- javascript.js
+++ javascript.js
@@ -3,3 +3,8 @@
   This contains javascript functions so the page can change dynamically
 */
 // global data structure to hold order
+
+function goToHTML(page){
+  var element = document.getElementById("changeHTML");
+  element.src = page;
+}
