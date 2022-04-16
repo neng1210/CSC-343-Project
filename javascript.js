442b8f6ed65e1be978a5aed072732666f17f088b
neng1210 <neng1210@email.arizona.edu>
2022-04-16 13:08:49 -0700

    Part of Checkout
    Part of checkout done. No thank you page yet.

--- javascript.js
+++ javascript.js
@@ -4,6 +4,31 @@
 */
 // global data structure to hold order
 
+// just some hard coded thing so i can test checkout
+// will probably be changed to different data struture later
+order = ["roll", "roll", "nigiri"]
+let prices = {}
+prices["roll"] = 4;
+prices["nigiri"] = 5;
+
+function displayOrder() {
+  string = "Your Order";
+  total = 0;
+  for(let i = 0; i < order.length; i++) {
+    string = string+ "<br>" + order[i] + "\t\t"+ prices[order[i]];
+    total = total + prices[order[i]];
+  }
+  string = string + "<br>"
+  totalStr = "Your Total is $" + total;
+  document.getElementById("order").innerHTML = string;
+  document.getElementById("total").innerHTML = totalStr;
+}
+
+function creditCard() {
+  string = "Insert a Credit Card."
+  document.getElementById("confirm").innerHTML = string;
+}
+
 function goToHTML(page){
   var element = document.getElementById("changeHTML");
   element.src = page;
