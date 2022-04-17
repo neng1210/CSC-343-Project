f28dfa3dbd9088058c0c11673b010a95f21a60c0
Firedorange717 <joshuapk1234@gmail.com>
2022-04-16 19:40:29 -0700

    work on welcome page

--- javascript.js
+++ javascript.js
@@ -2,7 +2,6 @@
   Authors: Nicholas Eng, Ryu Funakoshiya, Joshua Kantarges
   This contains javascript functions so the page can change dynamically
 */
-// global data structure to hold order
 
 function goToHTML(page){
   var element = document.getElementById("changeHTML");
