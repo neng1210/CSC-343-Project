3e6787564dea794ef08af3ce5b29b3cc277acd4e
RyuFunakoshiya <rfunakoshiya@email.arizona.edu>
2022-04-18 18:47:06 -0700

    Fixed up each and every selected item menu!!
    -Rolls, Handrolls, Nigiri, etc menu is now done! Touching up the looks now!

--- javascript.js
+++ javascript.js
@@ -13,19 +13,19 @@ var foodItems={
       "name": "Tuna Roll",
       "price": 4.99,
       "description": "A simple sushi roll with only raw tuna encased in sushi rice, rolled within one sheet of nori.",
-      "image": "images/rolls/tunaRoll.jpg"
+      "image": "../images/rolls/tunaRoll.jpg"
     },
     "pickledDaikonRoll":{
       "name": "Pickled Daikon Roll",
       "price": 3.99,
       "description": "Sweet and sourness of the pickled daikon mixed with the sushi rice and roasted nori sheet is not like anything else in the world",
-      "image": "images/rolls/pickledDaikonRoll.jpg"
+      "image": "../images/rolls/pickledDaikonRoll.jpg"
     },
     "futomaki":{
       "name": "Futomaki",
       "price": 6.99,
       "description": "Futomaki is like a normal maki roll but bigger.",
-      "image": "images/rolls/futomaki.jpg"
+      "image": "../images/rolls/futomaki.jpg"
     },
   },
   "handRolls":{
@@ -33,19 +33,19 @@ var foodItems={
       "name": "Temaki",
       "price": 8.99,
       "description": "These are hand rolls include scallions, cucumbers, tuna, salmon.",
-      "image": "images/handRolls/temaki.jpg"
+      "image": "../images/handRolls/temaki.jpg"
     },
     "spicyTuna":{
       "name": "Spicy Tuna",
       "price": 7.99,
       "description": "Sushi grade tuna with a spicy chili sauce, a hint of mayonnaise, toasted sesame seeds, and sliced cucumber make this fabulous tuna hand roll!",
-      "image": "images/handRolls/spicyTuna.jpg"
+      "image": "../images/handRolls/spicyTuna.jpg"
     },
     "softCrab":{
       "name": "Soft Crab",
       "price": 9.99,
       "description": "A perfectly fried soft shell crab that is crunchy and superb",
-      "image": "images/handRolls/softCrab.jpg"
+      "image": "../images/handRolls/softCrab.jpg"
     },
   },
   "nigiri":{
@@ -53,19 +53,19 @@ var foodItems={
       "name": "Regular Nigiri",
       "price": 4.99,
       "description": "Regular Nigiri sushi will be prepared the normal way, with the sliced fish on top of the rice",
-      "image": "images/nigiri/regNigiri.jpg"
+      "image": "../images/nigiri/regNigiri.jpg"
     },
     "gunkanMaki":{
       "name": "Gunkan Maki",
       "price": 5.50,
       "description": "Gunkan maki is a nigiri sushi that is surrounded by a sheet of nori (dried seaweed)",
-      "image": "images/nigiri/gunkanMaki.jpg"
+      "image": "../images/nigiri/gunkanMaki.jpg"
     },
     "noriRibbon":{
       "name": "Nori Ribbon",
       "price": 3.50,
       "description": "A thin strip of nori wrapped around the nigiri sushi",
-      "image": "images/nigiri/noriRibbon.jpg"
+      "image": "../images/nigiri/noriRibbon.jpg"
     },
   },
   "drinks":{
@@ -73,19 +73,19 @@ var foodItems={
       "name": "Water",
       "price": 0.00,
       "description": "A glass of water",
-      "image": "images/drinks/water.jpg"
+      "image": "../images/drinks/water.jpg"
     },
     "soda":{
       "name": "Soda",
       "price": 2.39,
       "description": "A Refreshing Fountian Drink",
-      "image": "images/drinks/fountian.jpg"
+      "image": "../images/drinks/fountian.jpg"
     },
     "tea":{
       "name": "Tea",
       "price": 1.39,
       "description": "Authentic Japanese Tea",
-      "image": "images/drinks/tea.jpg"
+      "image": "../images/drinks/tea.jpg"
     },
   },
   "desserts":{
@@ -93,19 +93,19 @@ var foodItems={
       "name": "Mochi",
       "price": 3.75,
       "description": "Japanese Mochi",
-      "image": "images/desserts/mochi.jpg"
+      "image": "../images/desserts/mochi.jpg"
     },
     "greenTea":{
       "name": "Green Tea Ice Cream",
       "price": 4.39,
       "description": "Japanese Green Tea Ice Cream",
-      "image": "images/desserts/greenTea.jpg"
+      "image": "../images/desserts/greenTea.jpg"
     },
     "taiyaki":{
       "name": "Taiyaki",
       "price": 2.79,
       "description": "Authentic Japanese Taiyaki",
-      "image": "images/desserts/taiyaki.jpg"
+      "image": "../images/desserts/taiyaki.jpg"
     },
   },
   "specials":{
@@ -113,19 +113,19 @@ var foodItems={
       "name": "Sushi Boat #1",
       "price": 13.79,
       "description": "Chefs Special #1",
-      "image": "images/specials/sushiBoat1.jpg"
+      "image": "../images/specials/sushiBoat1.jpg"
     },
     "sushiBoat2":{
       "name": "Sushi Boat #2",
       "price": 13.79,
       "description": "Chefs Special #2",
-      "image": "images/specials/sushiBoat2.jpg"
+      "image": "../images/specials/sushiBoat2.jpg"
     },
     "sushiBoat3":{
       "name": "Sushi Boat #3",
       "price": 13.79,
       "description": "Chefs Special #3",
-      "image": "images/specials/sushiBoat3.jpg"
+      "image": "../images/specials/sushiBoat3.jpg"
     },
   },
 }
@@ -205,19 +205,24 @@ function populateFoodPage(category){
       categoryItems = foodItems.specials;
       break;
   }
-
+  var posCounter = 0;
+  var position = ["topLeft","topMiddle","topRight","bottomLeft","bottomMiddle","bottomRight"];
+  var items = '';
   for(const food in categoryItems){
-    var newItem = `
-      <div id="foodEntry">
-        <a href="addItemToOrder(${categoryItems[food].name}, ${categoryItems[food].price})"><img src="./${categoryItems[food].image}"></a>
+    items += `
+      <div class="foodArea" id=${position[posCounter]}>
+        <a href="addItemToOrder(${categoryItems[food].name}, ${categoryItems[food].price})">
+          <img src="./${categoryItems[food].image}" id="images">
+        </a>
         <p>${categoryItems[food].name}</p>
         <p>$${categoryItems[food].price}</p>
         <p>${categoryItems[food].description}</p>
       </div>
     `;
-    document.getElementById("foodArea").innerHTML += newItem;
+    posCounter++;
   }
-
+  items += "";
+  document.getElementById("purchaseArea").innerHTML = items;
 }
 
 function addItemToOrder(name, price){
